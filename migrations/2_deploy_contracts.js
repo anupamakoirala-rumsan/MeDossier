3d0e3cfa8a7c6820ebd3012753b9f86337ffb6b0
anupamakoirala <anukoirala.96@hotmail.com>
2021-06-24 21:34:47 +0545

    Contract added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var meDossier = artifacts.require("./meDossier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(meDossier);
 };
