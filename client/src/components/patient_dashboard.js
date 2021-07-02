6d366572ca1d9e183f79cc6000e5ecbffce7c1d9
anupamakoirala <anukoirala.96@hotmail.com>
2021-07-02 10:07:52 +0545

    Verifier and patient's dashboard connected with blockchain

--- client/src/components/patient_dashboard.js
+++ client/src/components/patient_dashboard.js
@@ -3,16 +3,19 @@ import {Tabs, Tab, Row, Nav, Navbar,Card,Button} from "react-bootstrap";
 import {withStyles,makeStyles} from '@material-ui/core/styles';
 import {Table, TableBody,TableCell,TableContainer,TableHead,TableRow}from "@material-ui/core";
 import Paper from '@material-ui/core/Paper';
+import ipfs from "../ipfs";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../logo.png'
+import logo from "./logo.png"
 
 function Patient(props){
    const dnameRef = useRef();
    const reasonRef = useRef();
    const dateRef = useRef();
    const addressRef = useRef();
-    console.log(props.records);
-    console.log(props.patient);
+   const [ipfshash, setIpfshash] =useState();
+   const[buffer,setBuffer] = useState();
+  console.log(props.records);
+  console.log(props.patient);
     const doctorRef = useRef();
     const grantRef = useRef();
   const useStyles = makeStyles({
@@ -48,7 +51,22 @@ function Patient(props){
    }))(TableRow)
 
 
-    
+    const uploadrecord= async(dname,reason,date,address)=>{
+      try{
+        console.log("hh")
+        console.log(dname,reason,date);
+        console.log(props.currentAccount);
+        console.log(props.contract);
+         const res = await props.contract.methods.addRecord(dname,reason,date,ipfshash,props.currentAccount).send({from:props.currentAccount});
+        console.log(res);
+       await  props.getPatientRecord();
+
+      }catch(error){
+        alert(error)
+
+      }
+    }
+
     const Details = ()=>{
         return(
           <div className="Details">
@@ -59,21 +77,45 @@ function Patient(props){
 
             <Card > 
                     <Card.Body>
-                   <b> Name:</b>
+                   <b> Name:</b>{props.patient._name}
                   <br/>
-                   <b>Phone: </b>
+                   <b>Phone: </b>{props.patient._phone}
                   <br/>
-                  <b>Gender:</b> 
+                  <b>Gender:</b>{props.patient._gender}
                   <br/>
-                <b> Date of Birth:</b> 
+                <b> Date of Birth:</b>{props.patient._dob}
                 <br/> 
-               <b> Blood Group: </b> 
+               <b> Blood Group: </b>{props.patient._bloodgroup}
              </Card.Body>
             </Card>
             </div>
 
         )
     }
+const handlechange = async(event)=>{
+  event.preventDefault();
+            //capture the userfile 
+            const file = event.target.files[0];
+            //Read the file
+            let reader = new window.FileReader()
+            reader.readAsArrayBuffer(file)
+            //file is converted to a buffer to prepare for uploading to IPFS
+            reader.onloadend=()=>{
+                let buffer = Buffer(reader.result)
+                setBuffer(buffer);
+                console.log("buffer",buffer);
+            }
+
+}
+const onsubmit = async(event)=>{
+  event.preventDefault();
+  console.log("hh");
+  const ipfsHash = await ipfs.add(buffer);
+  console.log(ipfsHash);
+        console.log(ipfsHash[0].hash)
+        setIpfshash(ipfsHash[0].hash);
+
+}
 
     const Upload =()=>{
         return(
@@ -83,19 +125,24 @@ function Patient(props){
 
               <div className="upload">
                 <label> Upload your report to IPFS</label>
-    <form >
+    <form onSubmit={onsubmit}>
       <label> 
         </label>
-        <input type= "file"  
+        <input type= "file"  onChange ={handlechange}
        
         />
         <br/>
-         <Button >Submit</Button>
+         <Button onClick={onsubmit}>Submit</Button>
          </form>
          </div>
 
          {/* Upload file to blockchain */}
-         <form>
+         <form 
+        //  onSubmit= {(event)=>{
+        //    event.preventDefault();
+        //    uploadrecord();
+        //  }}
+         >
          <label> Upload your record to blockchain  </label><br/>
           Name:<input 
          type ="text"placeholder="Name of the doctor" 
@@ -115,7 +162,16 @@ function Patient(props){
          ref ={addressRef}/>
          <br/>
          {/* <input type ="submit"/> */}
-         <Button>Submit</Button>
+         <Button onClick={(event)=>{
+           event.preventDefault();
+
+           const dname = dnameRef.current.value;
+           const reason = reasonRef.current.value;
+           const date = dateRef.current.value;
+           const address = reasonRef.current.value
+
+           uploadrecord(dname,reason,date,address);
+         }}>Submit</Button>
          </form>
          </Card>
          </div>
@@ -124,17 +180,17 @@ function Patient(props){
 
   const Report =()=>{
     // props.getPatientRecord();
-    // if(props.records.length  === 0){
-    //   return(
-    //     <div> 
-    //       <h2>
-    //       Your Report
-    //     </h2>
-    //     <p> Your record will appear here.</p>
-    //      <p> loading........</p>
-    //     </div>
-    //   )
-    // }
+    if(props.records.length  === 0){
+      return(
+        <div> 
+          <h2>
+          Your Report
+        </h2>
+        <p> Your record will appear here.</p>
+         <p> loading........</p>
+        </div>
+      )
+    }
     
     return(
       <div className="Report">
@@ -155,19 +211,18 @@ function Patient(props){
                             <StyledTablecell>Records</StyledTablecell>
                         </TableRow>
                     </TableHead>
-                    {/* {props.records.map((record,key)=>(  */}
+                    {props.records.map((record,key)=>( 
 
                     <TableBody>
 
-                        <StyledTableRow >
-                          <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell> </TableCell>
-                        <TableCell><a href={`https://ipfs.io/ipfs/`} target="_blank"> click here to view your record</a></TableCell>
+                        <StyledTableRow key ={key}>
+                          <TableCell>{key}</TableCell>
+                        <TableCell>{record.dname}</TableCell>
+                        <TableCell>{record.reason}</TableCell>
+                        <TableCell>{record.visitedDate}</TableCell>
+                        <TableCell><a href={`https://ipfs.io/ipfs/${record.ipfs}`} target="_blank"> click here to view your record</a></TableCell>
                         </StyledTableRow>
-                    </TableBody>
-                    {/* ))} */}
+                    </TableBody>))}
                 </Table>
             </TableContainer>
             </Paper>
@@ -180,11 +235,22 @@ return(
     <h2>Grant/Revoke Access</h2>
     <Card>
 
-            <form >
+            <form 
+            // onSubmit ={(event)=>{
+            //     event.preventDefault();
+            //     const doctor = doctorRef.current.value;
+            //     props.grantAccess(doctor);
+            // }}
+            >
                 <label>Provide Access: </label>
                 <input type="text" placeholder=" Address to grant access"
                 ref ={grantRef}/> 
-                <Button> Submit</Button>
+                <Button onClick ={(event)=>{
+                  event.preventDefault();
+                  const doctor = grantRef.current.value;
+                  console.log(doctor)
+                  props.grantAccess(doctor);
+              }}> Submit</Button>
                 </form>
 
                 <br/>
@@ -198,7 +264,12 @@ return(
                     <label> Revoke Access: </label>
                     <input type="text" placeholder=" Address to revoke access from"
                     ref={doctorRef}/>
-                <Button > Submit</Button>
+                <Button onClick  ={(event)=>{
+                    event.preventDefault();
+                    const doctor = doctorRef.current.value;
+                    console.log(doctor);
+                    props.revokeAccess(doctor);
+                }}> Submit</Button>
                 </form>
                 </Card>
 
@@ -238,7 +309,7 @@ return(
                             <Nav.Item>
                             <Nav.Link eventKey="profile">AccessRecord</Nav.Link> <hr/></Nav.Item>                           
                             <Nav.Item><Nav.Link eventKey="uploadRecord">UploadRecord</Nav.Link><hr/></Nav.Item>
-                            <Nav.Item><Nav.Link eventKey="access">Grant/Revoke Acccess</Nav.Link><hr/>
+                            <Nav.Item><Nav.Link eventKey="access">Grant/Revoke Acccess</Nav.Link><hr/>f
                             </Nav.Item>
 
                             </Nav> 
